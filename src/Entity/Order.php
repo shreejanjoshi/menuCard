6eec582d8dd7991a889bee16a277b7c974d42a51
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-06 16:24:31 +0100

    made a new entity order

---
+++ src/Entity/Order.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $sit;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $ordernumber;
+
+    #[ORM\Column(type: 'float', nullable: true)]
+    private $price;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSit(): ?string
+    {
+        return $this->sit;
+    }
+
+    public function setSit(?string $sit): self
+    {
+        $this->sit = $sit;
+
+        return $this;
+    }
+
+    public function getOrdernumber(): ?string
+    {
+        return $this->ordernumber;
+    }
+
+    public function setOrdernumber(?string $ordernumber): self
+    {
+        $this->ordernumber = $ordernumber;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
