d8e943e4d3b80dc7775823e4d33ae52b5a87b926
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-06 10:44:00 +0100

    made a form to register users

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/registration', name: 'registration')]
-    public function index(): Response
+    #[Route('/reg', name: 'reg')]
+    public function reg(ManagerRegistry $doctrine, Request $request): Response
     {
+        $user = new User();
+
+        //form
+        $form = $this->createForm(RegistrationType::class, $user);
+        //to send data to database
+        $form->handleRequest($request);
+
+        //if submit
+        if($form->isSubmitted()){
+            //entity manager
+            $em = $doctrine->getManager();
+
+            $em->persist();
+            //to change in database
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('dish.edit'));
+        }
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'regform' => $form->createView()
         ]);
     }
 }
