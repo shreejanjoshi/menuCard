42e2837471339b3e5e9a850458b9e3c7400f7538
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-07 10:28:32 +0100

    trying to send mail so install package mailgun mailer smtp

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -4,15 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
 {
-    #[Route('/mailer', name: 'mailer')]
-    public function index(): Response
+    #[Route('/mail', name: 'mail')]
+    public function sendEmail(MailerInterface $mailer): Response
     {
-        return $this->render('mailer/index.html.twig', [
-            'controller_name' => 'MailerController',
-        ]);
+        $email = (new Email())
+            ->from('sit1@menucard.wip')
+            ->to('waiter@menucard.wip')
+            ->subject('Order')
+            ->text('extra Fries');
+
+        $mailer->send($email);
+
+        return new Response('email has been send successfully');
     }
 }
