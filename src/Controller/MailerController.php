208b8fbbe2e353cd0ffabd085e4642a65492559d
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-07 10:18:34 +0100

    made new controller mailer

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+    #[Route('/mailer', name: 'mailer')]
+    public function index(): Response
+    {
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+        ]);
+    }
+}
